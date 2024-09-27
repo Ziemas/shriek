ddef43953bca17674770992b1bdeaa82ee6ef462
Ziemas <ziemas@ziemas.se>
2024-09-28 01:57:13 +0200

    init2

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,14 @@
 #include <fmt/core.h>
 #include <libpsxav.h>
+#include <memory>
 
-int main()
+class AudioFile {
+public:
+    static std::unique_ptr<AudioFile> make_audio_file();
+private:
+};
+
+int main(int argc, char *argv[])
 {
     fmt::print("Hello World!\n");
     return 0;
